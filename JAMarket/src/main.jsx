40a6f961057c5afa73657879d87f7b05d28b4946
Karl <karlnino.bautista@tup.edu.ph>
2025-07-18 22:23:46 +0800

    made a products table

--- JAMarket/src/main.jsx
+++ JAMarket/src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
-import router from './components/router.jsx'
+import router from './components/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
