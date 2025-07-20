89bfdf639b218508a046c623cb28fa6e564bbc17
Karl <karlnino.bautista@tup.edu.ph>
2025-07-20 18:32:47 +0800

    made a add product route to my backend to my supabase db

--- Backend/index.js
+++ Backend/index.js
@@ -1,47 +1,17 @@
 const express = require("express");
-const path = require("path");
+const dotenv = require("dotenv");
 const cors = require("cors");
-require('dotenv').config();
-const supabase = require("./supabaseClient");
-
+dotenv.config();
 const app = express();
 const PORT = 5000;
 
+
+const productRoutes = require("./routes/ProductRoutes");
+
 app.use(cors());
-app.use(express.json());
+app.use(express.json());  
 
-
-app.post("/register", async (req, res) => {
-    const { fullname, email, password, user_type } = req.body;
-    console.log(fullname, email, password, user_type)
-    try{
-        const { data: authData, error: authError } = await supabase.auth.signUp({
-            email: email,
-            password: password
-        })
-
-        if(authError){
-            return res.status(500).json({ message: authError.message });
-        }
-        const user = authData.user;
-
-        const { error: insertError } = await supabase.from("users").insert({
-            id: user.id,
-            fullname,
-            user_type
-        }).single();
-
-        if(insertError){
-            return res.status(500).json({ message: insertError.message });
-        }
-       return res.status(200).json({ message: "user registered successfully" });
-    } catch(err){
-    }
-}) 
-
-
-
-
+app.use("/api", productRoutes);
 
 
 
