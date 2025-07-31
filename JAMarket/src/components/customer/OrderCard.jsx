c624136a3bde77ca76f7af32327ad584a51fe56f
Karl <karlbautista234@gmail.com>
2025-07-31 18:18:32 +0800

    partial development of the order structure

--- JAMarket/src/components/customer/OrderCard.jsx
+++ JAMarket/src/components/customer/OrderCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "../../css/OrderCard.css"
-const OrderCard = ({ order }) => {
+const OrderCard = ({ order, orderItem }) => {
   return (
     <div className='order-card-container'>
-        
+        {order.status}
       
     </div>
   )
